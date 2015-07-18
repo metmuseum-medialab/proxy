ec2dadf9185a00431759aadc02989db240601335
don undeen <don.undeen@metmuseum.org>
2015-07-18 19:41:54 -0400

    getting into ec2

--- server.node.js
+++ server.node.js
@@ -1,21 +1,37 @@
 var HttpMaster = require("http-master");
 var httpMaster = new HttpMaster();
 
-var config = require("./config.json");
+var config = require("./config.ec2.json");
 
 httpMaster.init(config,
  function(err){
 	// listening
-	console.log(err);
+	console.log("proxy running");
+	if(err){
+		console.log(err);
+	}
+    try {
+        console.log('Old User ID: ' + process.getuid() + ', Old Group ID: ' + process.getgid());
+        process.setgid('staff');
+        process.setuid('donundeen');
+        console.log('New User ID: ' + process.getuid() + ', New Group ID: ' + process.getgid());
+    } catch (err) {
+        console.log('Cowardly refusing to keep the process alive as root.');
+        console.log(err);
+        process.exit(1);
+    }
+
 });
 
 httpMaster.logNotice(function(msg){
+    console.log("logging notice");
 	console.log(msg);
 });
 
-
-
 httpMaster.logError(function(msg){
-	console.log(msg);
+    console.log("logging error");
+	if(msg){
+		console.log(msg);
+	}
 });
 
